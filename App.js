e879268e1f1ed1d8a40082b813ed38b07c8c4c5e
Ezequias <ezequiasmartins@gmail.com>
2023-12-30 19:20:10 -0300

    30122023-1920

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useMemo, useRef } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import BottomSheet from '@gorhom/bottom-sheet';
 
 export default function App() {
+  const bottomSheetRef = useRef(null);
+  const snapPoints = useMemo(() => ["30%", "70%"], []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        {/* <Text style={styles.title}>Sujeito Programador!</Text>
+        <Text style={styles.subtitle}>Action Buttom Sheet</Text> */}
+        <BottomSheet
+          ref={bottomSheetRef}
+          index={1}
+          snapPoints={snapPoints}
+          backgroundStyle={{ backgroundColor: "#FFF"}}
+        >
+          <View style={styles.contentBox}>
+            <Text>Conteúdo do Action Bottom Sheet</Text>
+            <Button title="Fehar" onPress={()=>{}}/>
+          </View>
+        </BottomSheet>
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    justifyContent: "flex-start",
+    alignItems: "center",
   },
+  contentBox:{
+    flex: 1,
+    alignItems: "center",
+  },
+  title: {
+    color: "#FF9900", 
+    fontWeight: "normal",
+    fontSize: 18,
+    marginTop:40
+  },
+  subtitle: {
+    color: "#FFF", 
+    fontWeight: "normal",
+    fontSize: 12,
+  }
 });
